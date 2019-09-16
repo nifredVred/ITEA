b0132cf041beb0a06df8eb27903a4aa0da26518b
Kapustin Ruslan <nifred1987@gmail.com>
2019-09-16 19:15:28 +0300

    first pull request

--- HomeWork2.cpp
+++ HomeWork2.cpp
@@ -4,28 +4,70 @@
 
 #include <iostream>
 #include <Limits>
+#include <cmath>
 
 
 int main(int arg, char const* argv[])
 {
-	short a;
-	int b;
-	long c;
-	long long e;
+	constexpr int Integer_Max_Value = std::numeric_limits<int>::max();
+	constexpr int Integer_Min_Value = std::numeric_limits<int>::min();
+	double a, b, c;
 
-	char ch = 25;
-	wchar_t ch1 = 33;
+	std::cout << "C++ Program to Solve Quadratic" << std::endl
+		<< "Please enter coefficients: a, b and c in rang ["
+		<< Integer_Max_Value << " to " << Integer_Min_Value << "] to solve the square equation\n" << std::endl
+		<< "Please enter a" << std::endl;
 
-	double f;
-	long double g;
-	/// ieee 754
+	std::cin >> a;
+
+	if (a != 0 && a >= Integer_Min_Value && a <= Integer_Max_Value) {
+
+		std::cout << std::endl << "Please enter b" << std::endl;
+		std::cin >> b;
+
+		if (b >= Integer_Min_Value && b <= Integer_Max_Value) {
+
+			std::cout << std::endl << "Please enter c" << std::endl;
+			std::cin >> c;
+
+			if (c >= Integer_Min_Value && c <= Integer_Max_Value) {
+
+				const unsigned int NUMBER_2 = 2;
+				const unsigned int NUMBER_4 = 4;
+				const auto D = std::pow(b, 2) - NUMBER_4 * a * c;
+
+				if (D == 0) {
+					auto x_1 = (-b) / (NUMBER_2 * a);
+					auto x_2 = x_1;
+
+					std::cout << "There is only one real root x1 = x2 = " << x_1 << "\n";
+				}
+
+				else if (D > 0) {
+					auto x_1 = (-b - std::sqrt(D)) / (NUMBER_2 * a);
+					auto x_2 = (-b + std::sqrt(D)) / (NUMBER_2 * a);
+
+					std::cout << "There are two real roots x1 = " << x_1 << " and x2 = " << x_2 << std::endl;
+				}
+
+				else if (D < 0) {
+
+					std::cout << "This equation does not have a solution" << "\n";
+
+				}
+			}
+			else std::cout << "c dose not fit the range from" << Integer_Min_Value << " to " << Integer_Max_Value << "\n";
+
+		}
+		else std::cout << "b dose not fit the range from" << Integer_Min_Value << " to " << Integer_Max_Value << "\n";
+
+	}
+	else std::cout << "a dose not fit the range from" << Integer_Min_Value << " to " << Integer_Max_Value << "\n";
 
 
-	std::cout << sizeof(a) << " "  << sizeof(b) << " " << sizeof(c) << " ";
-	std::cout << '\n' << std::numeric_limits<short>::min() << std::endl;
 
 
-	//переменные универсального типа 
+
 	return 0;
 }
 
